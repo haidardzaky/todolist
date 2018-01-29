fa316fe7ae30fdcffa369c0df16e2173c9688a67
haidardzaky <haidardz@gmail.com>
2018-01-29 10:09:40 +0700

    adding files

---
+++ index.js
@@ -0,0 +1,52 @@
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var button = document.createElement("button");
+  var txt = document.createTextNode("\u00D7");
+  button.className = "close";
+  button.appendChild(txt);
+  myNodelist[i].appendChild(button);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("to-do-list").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myol").appendChild(li);
+  }
+  document.getElementById("to-do-list").value = "";
+
+  var button = document.createElement("button");
+  var txt = document.createTextNode("\u00D7");
+  button.className = "close";
+  button.appendChild(txt);
+  li.appendChild(button);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+var submit = document.getElementById("button1");
+//var close = document.getElementsByClassName("close");
+
+submit.addEventListener("click", newElement);
+//close.addEventListener("click", deletedata);
